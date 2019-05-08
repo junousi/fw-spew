ddadbc87157819678bfa1a3d5a8701a558b83c21
Jukka Nousiainen <jukka.nousiainen@csc.fi>
2019-05-08 14:28:48 +0300

    Operate on dicts with fw rules

--- csv2fw.py
+++ csv2fw.py
@@ -3,16 +3,12 @@ import csv
 import socket
 import string
 
-from junos_fw import junos_fw_rule
+import junos_fw
+
+from collections import defaultdict
 
 TERM_PREFIX = 'term-'
 INDENT = 4
-
-def indent(s, spaces):
-    s = string.split(s, '\n')
-    s = [(spaces * ' ') + line for line in s]
-    s = string.join(s, '\n')
-    return s
 
 # Yes. argparse.
 if len(sys.argv) > 2:
@@ -20,25 +16,61 @@ if len(sys.argv) > 2:
 if len(sys.argv) > 3:
     INDENT = int(sys.argv[3])
 
-with open(sys.argv[1]) as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=',')
-    line_count = 0
-    for idx, row in enumerate(csv_reader):
-        src_cidr = row[0]
-        dst_cidr = row[1]
-        protocol = row[2]
-        ports = row[3]
-        src_fqdn = ''
-        dst_fqdn = ''
-        if '/32' in src_cidr:
-            src_fqdn = socket.gethostbyaddr(src_cidr.split('/')[0])[0]
-        if '/32' in dst_cidr:
-            dst_fqdn = socket.gethostbyaddr(dst_cidr.split('/')[0])[0]
-        rule = junos_fw_rule(TERM_PREFIX + str(idx),
-                            src_fqdn,
-                            src_cidr,
-                            dst_fqdn,                            
-                            dst_cidr,
-                            protocol,
-                            ports)
+def indent(s, spaces):
+    s = string.split(s, '\n')
+    s = [(spaces * ' ') + line for line in s]
+    s = string.join(s, '\n')
+    return s
+
+def test_read_csv_and_print(f):
+    with open(f) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        for idx, row in enumerate(csv_reader):
+            src_cidr = row[0]
+            dst_cidr = row[1]
+            protocol = row[2]
+            ports = row[3]
+            rule = junos_fw.rule_single(TERM_PREFIX + str(idx),
+                                src_cidr,
+                                dst_cidr,
+                                protocol,
+                                ports)
+            print indent(rule, INDENT)
+
+def csv_to_dict(f):
+    rules = defaultdict(list)
+    with open(f) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        for idx, row in enumerate(csv_reader):
+            src_cidr = row[0]
+            dst_cidr = row[1]
+            src_fqdn = ''
+            dst_fqdn = ''
+            protocol = row[2]
+            dst_ports = row[3]
+            if '/32' in src_cidr:
+                try:
+                    src_fqdn = socket.gethostbyaddr(src_cidr.split('/')[0])[0]
+                except:
+                    # No PTR returned from query.
+                    pass
+            if '/32' in dst_cidr:
+                try:
+                    dst_fqdn = socket.gethostbyaddr(dst_cidr.split('/')[0])[0]
+                except:
+                    pass
+            rules[(dst_cidr,protocol,dst_ports)].append(src_cidr)
+    return rules
+
+def dict_to_junos(rules):
+    rules_formatted = junos_fw.rule_from_dict(TERM_PREFIX, rules)
+    return rules_formatted
+
+if __name__ == '__main__':
+    #read_csv_and_print(sys.argv[1])
+    rules = csv_to_dict(sys.argv[1])
+    rules_formatted = dict_to_junos(rules)
+    for rule in rules_formatted:
         print indent(rule, INDENT)
--- junos_fw.py
+++ junos_fw.py
@@ -1,6 +1,21 @@
-#Creates a simple junos firewall rule.
+#Creates junos firewall rules.
+import socket
 
-def junos_fw_rule(term, src_fqdn, src_cidr, dst_fqdn, dst_cidr, protocol, dst_port_list):
+def rule_single(term, src_cidr, dst_cidr, protocol, dst_port_list):
+
+    src_fqdn = ''
+    dst_fqdn = ''
+    if '/32' in src_cidr:
+        try:
+            src_fqdn = socket.gethostbyaddr(src_cidr.split('/')[0])[0]
+        except:
+            pass
+    if '/32' in dst_cidr:
+        try:
+            dst_fqdn = socket.gethostbyaddr(dst_cidr.split('/')[0])[0]
+        except:
+            pass
+    
     rule = '''\
 term {term} {{
     from {{
@@ -23,5 +38,60 @@ then accept;
             dst_cidr=dst_cidr,
             protocol=protocol,
             dst_port_list=dst_port_list,)
-    
     return rule
+
+
+def rule_from_dict(term, rules):
+    
+    rules_formatted = []
+
+    for key in rules.keys():
+        
+        src_fqdn = ''
+        dst_fqdn = ''
+
+        src_cidr_list = rules[key]
+        src_cidr_lines = ''
+        for cidr in src_cidr_list:
+            src_cidr_lines = src_cidr_lines + ' ' * 12 + str(cidr) + '\n'
+
+        dst_cidr = key[0]
+        protocol = key[1]
+        dst_port_list = key[2]
+
+        # if '/32' in src_cidr:
+        #     try:
+        #         src_fqdn = socket.gethostbyaddr(src_cidr.split('/')[0])[0]
+        #     except:
+        #         pass
+        if '/32' in dst_cidr:
+            try:
+                dst_fqdn = socket.gethostbyaddr(dst_cidr.split('/')[0])[0]
+            except:
+                pass
+
+        rule = '''\
+term {term} {{
+    from {{
+        source-address {{
+            /* {src_fqdn} */
+{src_cidr_lines}
+        }}
+        destination-address {{
+            /* {dst_fqdn} */
+            {dst_cidr};
+        }}
+        protocol {protocol};
+        destination-port [ {dst_port_list} ];
+    }}
+then accept;
+}}'''.format(term=term,
+            src_fqdn=src_fqdn,
+            src_cidr_lines=src_cidr_lines,
+            dst_fqdn=dst_fqdn,
+            dst_cidr=dst_cidr,
+            protocol=protocol,
+            dst_port_list=dst_port_list,)
+        rules_formatted.append(rule)
+
+    return rules_formatted
